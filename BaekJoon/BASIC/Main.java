e33c99e3b6f82dd2fe721960febcf9aeeb688d39
hj5o <decopic@naver.com>
2022-02-04 13:47:02 +0900

    JAVA basic Baekjoon 6588

--- BaekJoon/BASIC/Main.java
+++ BaekJoon/BASIC/Main.java
@@ -1,6 +1,77 @@
 package BaekJoon.BASIC;
 
-// 과제 골드바흐의 추측 6588
+//// 과제 골드바흐의 추측 6588
+//import java.io.*;
+//import java.util.StringTokenizer;
+//
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//        int max = 1000000;
+//
+//        boolean[] check = new boolean[max + 1];
+//
+//        check[0] = check[1] = true;
+//
+//        for (int i = 2; i * i <= max; i++) {
+//            if (check[i]) continue;
+//            for (int j = i + i; j <= max; j += i) {
+//                check[j] = true;
+//            }
+//        }
+//        String input = "";
+//        while (!(input = br.readLine()).equals("0")) {
+//            int end = Integer.parseInt(input);
+//            boolean bool = false;
+//            for (int i = 3; i <= end; i++) {
+//                if (!check[i] && !check[end - i]) {
+//                    bw.write(end + " = " + i + " + " + (end - i) + "\n");
+//                    bool = true;
+//                    break;
+//                }
+//            }
+//            if(!bool) bw.write("Goldbach's conjecture is wrong.");
+//        }
+//        bw.flush();
+//        bw.close();
+//    }
+//}
+
+////      1929 소수 구하기 (Buffer)
+//import java.io.*;
+//import java.util.StringTokenizer;
+//
+//
+//
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+//        int start = Integer.parseInt(st.nextToken());
+//        int end = Integer.parseInt(st.nextToken());
+//
+//        boolean[] check = new boolean[end+1];
+//
+//        check[0] = check[1] = true;
+//
+//        for(int i = 2; i*i <= end; i++){
+//            if(check[i]) continue;
+//            for(int j = i*i; j <= end; j+=i){
+//                check[j] = true;
+//            }
+//        }
+//        for(int i = start; i <= end; i++){
+//            if(!check[i]) bw.write(i + "\n");
+//        }
+//        bw.flush();
+//        bw.close();
+//    }
+//}
+
 
 ////      1929 소수 구하기
 //import java.util.*;
