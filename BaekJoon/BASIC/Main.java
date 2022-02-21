4c9ec1354d1d43a4d2ecb895ca9d00f2a49bc9b5
hj5o <dataliteracy@naver.com>
2022-02-21 22:48:35 +0900

    add

--- BaekJoon/BASIC/Main.java
+++ BaekJoon/BASIC/Main.java
@@ -1,3 +1,25 @@
 package BaekJoon.BASIC;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        int dis = 0;
+
+        for (int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+
+    }
+}
--- BaekJoon/BASIC/sort.java
+++ BaekJoon/BASIC/sort.java
@@ -18,3 +18,29 @@ package BaekJoon.BASIC;
 //        }
 //    }
 //}
+
+////     Problem 2751 수 정렬하기 2
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.ArrayList;
+//import java.util.Collections;
+//
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//        int N = Integer.parseInt(br.readLine());
+//        ArrayList<Integer> list = new ArrayList<>();
+//
+//        for (int i=0; i<N; i++) {
+//            list.add(Integer.parseInt(br.readLine()));
+//        }
+//        Collections.sort(list);
+//
+//        for (int num : list) {
+//            sb.append(num).append("\n");
+//        }
+//        System.out.println(sb);
+//    }
+//}
