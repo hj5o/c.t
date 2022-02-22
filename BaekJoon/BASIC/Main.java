8dcf036c53bb05e74358fcedd0713db654367c0d
hj5o <dataliteracy@naver.com>
2022-02-22 17:36:34 +0900

    add

--- BaekJoon/BASIC/Main.java
+++ BaekJoon/BASIC/Main.java
@@ -1,25 +1,30 @@
 package BaekJoon.BASIC;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        int N = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
-        int dis = 0;
-
-        for (int i=0; i<N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(arr);
-
-
-    }
-}
+////     Problem 10610 30
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.Arrays;
+//
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String str = br.readLine();
+//        int sum = 0;
+//
+//        char[] charArr = str.toCharArray();
+//        Arrays.sort(charArr);
+//        int len = charArr.length;
+//
+//        StringBuilder sb = new StringBuilder();
+//        for (int i=len-1; i>=0; i--) {
+//            int num = charArr[i] - '0';
+//            sum += num;
+//            sb.append(num);
+//        }
+//        if (charArr[0] != '0' || sum % 3 != 0) {
+//            System.out.println(-1);
+//            return;
+//        }
+//        System.out.println(sb.toString());
+//    }
+//}
--- BaekJoon/BASIC/math.java
+++ BaekJoon/BASIC/math.java
@@ -470,6 +470,36 @@ package BaekJoon.BASIC;
 //    }
 //}
 
+//     Problem 10610 30
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.Arrays;
+//
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String str = br.readLine();
+//        int sum = 0;
+//
+//        char[] charArr = str.toCharArray();
+//        Arrays.sort(charArr);
+//        int len = charArr.length;
+//
+//        StringBuilder sb = new StringBuilder();
+//        for (int i=len-1; i>=0; i--) {
+//            int num = charArr[i] - '0';
+//            sum += num;
+//            sb.append(num);
+//        }
+//        if (charArr[0] != '0' || sum % 3 != 0) {
+//            System.out.println(-1);
+//            return;
+//        }
+//        System.out.println(sb.toString());
+//    }
+//}
+
 ////     Problem  10818 최소, 최대
 //import java.io.BufferedReader;
 //import java.io.IOException;
