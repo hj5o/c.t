4e01f30e9e40d07a214605c621332ab008378fd8
hj5o <dataliteracy@naver.com>
2022-03-08 17:38:58 +0900

    11726 2*n 타일링

--- BaekJoon/BASIC/Main.java
+++ BaekJoon/BASIC/Main.java
@@ -1,5 +1,2 @@
 package BaekJoon.BASIC;
 
-
-
-
--- BaekJoon/BASIC/dynamic_programming.java
+++ BaekJoon/BASIC/dynamic_programming.java
@@ -53,4 +53,33 @@ public class Main {
         }
     }
 }
+       Problem 11726 2*n 타일링
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+1은 1가지
+2는 2가지
+3은 3가지
+4는 5가지
+dp[i] = dp[i-1] + dp[i-2]
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int max = 1000;
+        int[] dp = new int[max+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i=3; i<=N; i++) {
+            dp[i] = (dp[i-1] + dp[i-2])%10007;
+        }
+        System.out.println(dp[N]);
+    }
+}
 */
