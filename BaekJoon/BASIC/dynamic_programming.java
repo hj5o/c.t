f4dfc86587c9d4081f9a3124a7e0670e0189916f
hj5o <dataliteracy@naver.com>
2022-03-10 11:54:59 +0900

    2156 포도주 시식

--- BaekJoon/BASIC/dynamic_programming.java
+++ BaekJoon/BASIC/dynamic_programming.java
@@ -24,14 +24,50 @@ public class Main {
     }
 }
 
+       Problem 2156 포도주 시식
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+계단 오르기 문제와 비슷하다.
+다만 계단 오르기는 마지막 계단을 반드시 밟아야했는데, 포도주 시식문제는 그런 조건 없다.
+따라서 이전 dp와 비교하여 최댓값을 갱신해야한다.
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N];
+        int[] arr = new int[N];
+
+        for (int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        dp[0] = arr[0];
+        dp[1] = arr[0] + arr[1];
+        dp[2] = Math.max(dp[1], Math.max(dp[0] + arr[2], arr[1] + arr[2]));
+        for (int i=3; i<N; i++) {
+            dp[i] = Math.max(dp[i-1], Math.max(dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i]));
+        }
+        System.out.println(dp[N-1]);
+    }
+}
+
        Problem 2579 계단 오르기
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 N번째 계단을 밟는 경우? (연속된 계단 3개를 밟을 수 없음)
-2. N-2를 밟고 N을 밟는다.
-1. N-3과 N-1을 밟고 N을 밟는다.
+  1. N-2를 밟고 N을 밟는다.
+  2. N-3과 N-1을 밟고 N을 밟는다.
+*마지막 계단을 반드시 밟는다.
 
 import java.io.BufferedReader;
 import java.io.IOException;
