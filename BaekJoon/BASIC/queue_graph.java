08157c5ae3af9449e1624ed103ba2a0390a368da
hj5o <dataliteracy@naver.com>
2022-03-11 10:18:39 +0900

    9012 괄호

--- BaekJoon/BASIC/queue_graph.java
+++ BaekJoon/BASIC/queue_graph.java
@@ -1,5 +1,48 @@
 package BaekJoon.BASIC;
 /*
+       Problem 9012 괄호
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Character> stack = new Stack<>();
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<N; i++) {
+            String str = br.readLine();
+            int length = str.length();
+
+            for (int j=0; j<length; j++) {
+                char ch = str.charAt(j);
+                if (stack.isEmpty()) {
+                    stack.push(ch);
+                } else {
+                    if (ch == ')') {
+                        if (stack.peek() == '(') {
+                            stack.pop();
+                        }
+                        else {
+                            stack.push(ch);
+                        }
+                    } else {
+                        stack.push(ch);
+                    }
+                }
+            }
+            if (stack.isEmpty()) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+            stack.clear();
+        }
+    }
+}
+
        Problem 10773 제로
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
