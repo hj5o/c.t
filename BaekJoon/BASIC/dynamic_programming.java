1a9261214e2f94a3c1a6c6fbe74098eed2e86366
hj5o <dataliteracy@naver.com>
2022-03-18 13:35:31 +0900

    14501 퇴사

--- BaekJoon/BASIC/dynamic_programming.java
+++ BaekJoon/BASIC/dynamic_programming.java
@@ -228,5 +228,39 @@ public class Main {
         System.out.println(dp[N]);
     }
 }
+       Problem 14501 퇴사
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+N+1일에 퇴사하기 때문에
+t[i] + i는 N일 이하여야 한다.
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[] t = new int[N];
+        int[] p = new int[N];
+
+
+        for (int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine()," ");
+            t[i] = Integer.parseInt(st.nextToken());
+            p[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] dp = new int[N+1];
+
+        for (int i=0; i<N; i++) {
+            if (i + t[i] <= N){
+                dp[i + t[i]] = Math.max(dp[i + t[i]], dp[i] + p[i]);
+            }
+            dp[i+1] = Math.max(dp[i+1], dp[i]);
+        }
+        System.out.println(dp[N]);
+    }
+}
 */
 
