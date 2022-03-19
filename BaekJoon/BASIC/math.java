85f57ddfd89c57d8a6c32451d4deb6e6efd56683
hj5o <dataliteracy@naver.com>
2022-03-19 22:12:57 +0900

    14681 사분면 고르기

--- BaekJoon/BASIC/math.java
+++ BaekJoon/BASIC/math.java
@@ -663,6 +663,25 @@ public class Main {
         }
     }
 }
+       Problem 14681 사분면 고르기
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        if (x > 0 && y > 0) {
+            System.out.println(1);
+        } else if (x < 0 && y > 0) {
+            System.out.println(2);
+        } else if (x < 0 && y < 0) {
+            System.out.println(3);
+        } else {
+            System.out.println(4);
+        }
+    }
+}
 
        Problem  17427 약수 (2)
 import java.io.BufferedReader;
