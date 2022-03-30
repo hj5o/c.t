2b7b5de93d6ecf844db1d14b89e7b097a497a701
hj5o <dataliteracy@naver.com>
2022-03-30 18:42:50 +0900

    1149 RGB 거리

--- BaekJoon/BASIC/dynamic_programming.java
+++ BaekJoon/BASIC/dynamic_programming.java
@@ -31,6 +31,30 @@ public class Main {
         }
     }
 }
+       Problem 1149 RGB 거리
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] cost = new int[N][3];
+
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<3; j++) {
+                cost[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i=1; i<N; i++) {
+            cost[i][0] += Math.min(cost[i-1][1], cost[i-1][2]);
+            cost[i][1] += Math.min(cost[i-1][0], cost[i-1][2]);
+            cost[i][2] += Math.min(cost[i-1][0], cost[i-1][1]);
+        }
+        System.out.println(Math.min(Math.min(cost[N-1][0], cost[N-1][1]), cost[N-1][2]));
+    }
+}
 
        Problem 1463 1로 만들기
 import java.io.BufferedReader;
