78a492ff5f25c9bb601f540cecc3f1cfcee743b4
hj5o <dataliteracy@naver.com>
2022-03-31 19:31:20 +0900

    4153 직각삼각형

--- BaekJoon/BASIC/Main.java
+++ BaekJoon/BASIC/Main.java
@@ -1,10 +1,12 @@
 package BaekJoon.BASIC;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     }
 }
--- BaekJoon/BASIC/math.java
+++ BaekJoon/BASIC/math.java
@@ -513,6 +513,35 @@ public class Main {
         System.out.println(x + " " + y);
     }
 }
+       Problem 4153 직각삼각형
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[3];
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            arr[0] = Integer.parseInt(st.nextToken());
+            arr[1] = Integer.parseInt(st.nextToken());
+            arr[2] = Integer.parseInt(st.nextToken());
+
+            Arrays.sort(arr);
+
+            if (arr[0] == 0 && arr[1] == 0 && arr[2] == 0) {
+                break;
+            } else if (Math.pow(arr[2], 2) == (Math.pow(arr[0],2) + Math.pow(arr[1], 2))) {
+                System.out.println("right");
+            } else {
+                System.out.println("wrong");
+            }
+        }
+    }
+}
 
        Problem 4344 평균은 넘겠지
 import java.io.*;
