19aef106182af7eb05426519f2ca9f609cb949e7
hj5o <dataliteracy@naver.com>
2022-04-04 14:08:30 +0900

    2577 숫자의 개수

--- BaekJoon/BASIC/math.java
+++ BaekJoon/BASIC/math.java
@@ -316,8 +316,6 @@ public class Main {
 import java.io.*;
 import java.util.StringTokenizer;
 
-
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -458,6 +456,31 @@ public class Main {
             m -= 45;
         }
         System.out.println(h + " " + m);
+    }
+}
+
+       Problem 2577 숫자의 개수
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int mul = A*B*C;
+        int[] arr = new int[10];
+
+        while (mul != 0) {
+            arr[mul%10]++;
+            mul/=10;
+        }
+
+        for (int ans : arr) {
+            System.out.println(ans);
+        }
     }
 }
 
