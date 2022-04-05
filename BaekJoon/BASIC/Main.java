f21e5cdab8542b88c74daa2cb24ac3a676b9158c
hj5o <dataliteracy@naver.com>
2022-04-05 20:11:59 +0900

    2752 세수정렬

--- BaekJoon/BASIC/Main.java
+++ BaekJoon/BASIC/Main.java
@@ -1,7 +1,2 @@
 package BaekJoon.BASIC;
 
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- BaekJoon/BASIC/math.java
+++ BaekJoon/BASIC/math.java
@@ -545,6 +545,28 @@ public class Main {
         System.out.println(l);
     }
 }
+       Problem 2752 세수정렬
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[3];
+        for (int i=0; i<arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        for (int i : arr) {
+            System.out.print(i+ " ");
+        }
+    }
+}
 
        Problem  3009 네 번째 점
 import java.io.BufferedReader;
