399e96e9b6aab9e975835d83ad7e14ca824eeb00
hj5o <dataliteracy@naver.com>
2022-04-15 00:19:10 +0900

    16435 스네이크버드

--- BaekJoon/BASIC/sort.java
+++ BaekJoon/BASIC/sort.java
@@ -317,4 +317,35 @@ public class Main {
     }
 }
 
+       Problem 16435 스네이크버드
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        int[] fruits = new int[N];
+
+        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
+        for (int i=0; i<N; i++) {
+            fruits[i] = Integer.parseInt(st2.nextToken());
+        }
+        Arrays.sort(fruits);
+        for (int i=0; i< fruits.length; i++) {
+            if (fruits[i] <= L) {
+                L++;
+            } else {
+                break;
+            }
+        }
+        System.out.println(L);
+    }
+}
+
 */
