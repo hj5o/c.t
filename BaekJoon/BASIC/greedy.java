1d10dad37dd70aa982014a8c5384785fc9e1cf45
hj5o <dataliteracy@naver.com>
2022-04-21 20:09:24 +0900

    5179 안테나

--- BaekJoon/BASIC/greedy.java
+++ BaekJoon/BASIC/greedy.java
@@ -71,6 +71,32 @@ public class Main {
     }
 }
 
+       Problem 5179 안테나
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0; i<arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        if (N % 2 == 0) {
+            System.out.println(arr[N/2 - 1]);
+        } else {
+            System.out.println(arr[N/2]);
+        }
+    }
+}
+
        Problem 9327 이장님 초대
 import java.io.*;
 import java.util.*;
