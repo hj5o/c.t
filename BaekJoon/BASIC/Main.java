73d46158cf1b13964c2a568eb4d69c90463c80cf
hj5o <dataliteracy@naver.com>
2022-05-17 12:58:19 +0900

    11536 줄 세우기

--- BaekJoon/BASIC/Main.java
+++ BaekJoon/BASIC/Main.java
@@ -1,2 +1,28 @@
 package BaekJoon.BASIC;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] str = new String[N];
+        for (int i=0; i<N; i++) {
+            str[i] = br.readLine();
+        }
+        String[] clone = str.clone();
+        Arrays.sort(str);
+        if (clone[clone.length-1] == str[0]) {
+            System.out.println("DECREASING");
+        }
+        else if (clone[0] == str[0]) {
+            System.out.println("INCREASING");
+        }
+        else {
+            System.out.println("NEITHER");
+        }
+    }
+}
