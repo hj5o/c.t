831323f0e6315d628364dced665eea8e4f1aed6b
hj5o <dataliteracy@naver.com>
2022-05-27 22:11:16 +0900

    프로그래머스 Level_1

---
+++ Programmers/LEVEL_1/하샤드_수.java
@@ -0,0 +1,28 @@
+/*
+package Programmers.LEVEL_1;
+
+class Solution {
+    public boolean solution(int x) {
+        String[] arr = String.valueOf(x).split("");
+        int sum = 0;
+
+        for (String s : arr) {
+            sum += Integer.parseInt(s);
+        }
+
+        if (x % sum == 0) {
+            return true;
+        } else {
+            return false;
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int x = 11;
+        System.out.println("x = " + s.solution(x));
+    }
+}
+*/
