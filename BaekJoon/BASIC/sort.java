2c589e529a87f6ec287827144d663887b6fe5845
hj5o <dataliteracy@naver.com>
2022-05-31 23:41:37 +0900

    5800(성적 통계)

--- BaekJoon/BASIC/sort.java
+++ BaekJoon/BASIC/sort.java
@@ -292,6 +292,31 @@ public class Main {
     }
 }
 
+       Problem 5800 성적 통계
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i=0; i<N; i++) {
+            int a = sc.nextInt();
+            int max = Integer.MIN_VALUE;
+            int[] arr = new int[a];
+            for (int j=0; j<arr.length; j++) {
+                arr[j] = sc.nextInt();
+            }
+            Arrays.sort(arr);
+            for (int j=0; j<arr.length-1; j++) {
+                max = Math.max(max, arr[j+1] - arr[j]);
+            }
+            System.out.println("Class " + (i+1));
+            System.out.println("Max " + arr[arr.length-1] + ", Min " + arr[0] + ", Largest gap " + max);
+        }
+    }
+}
+
        Problem 10815 숫자 카드
 import java.io.BufferedReader;
 import java.io.IOException;
