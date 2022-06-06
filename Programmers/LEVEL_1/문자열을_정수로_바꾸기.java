656c82eac05ff9e78435ba97982225276dffde3a
hj5o <dataliteracy@naver.com>
2022-06-06 18:18:50 +0900

    프로그래머스 Level_1

---
+++ Programmers/LEVEL_1/문자열을_정수로_바꾸기.java
@@ -0,0 +1,16 @@
+/*
+package Programmers.LEVEL_1;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution("-1234"));
+    }
+}
+*/
---
+++ Programmers/LEVEL_1/자연수_뒤집어_배열로_만들기.java
@@ -0,0 +1,23 @@
+/*
+package Programmers.LEVEL_1;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(long n) {
+        String[] s = String.valueOf(n).split("");
+        int[] answer = new int[s.length];
+
+        for (int i=0; i<s.length; i++) {
+            answer[i] = Integer.parseInt(s[s.length-1-i]);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.solution(1206)));
+    }
+}
+*/
