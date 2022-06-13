ca11ef950dd5659681d48f66bf8d380cca0e04d4
hj5o <dataliteracy@naver.com>
2022-06-13 18:41:33 +0900

    프로그래머스 Level_1

---
+++ Programmers/LEVEL_1/없는_숫자_더하기.java
@@ -0,0 +1,19 @@
+/*
+package Programmers.LEVEL_1;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        for (int i=0; i<numbers.length; i++) {
+            answer += numbers[i];
+        }
+        return 45-answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] numbers = {1,2,3,4,6,7,8,0};
+        System.out.println(s.solution(numbers));
+    }
+}
+*/
