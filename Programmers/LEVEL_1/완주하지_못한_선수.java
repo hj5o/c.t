1a96646533a37a3abb7f42d89b54481869aad379
hj5o <dataliteracy@naver.com>
2022-06-16 20:26:52 +0900

    프로그래머스 Level_1

--- Programmers/LEVEL_1/완주하지_못한_선수.java
+++ Programmers/LEVEL_1/완주하지_못한_선수.java
@@ -5,26 +5,23 @@ import java.util.Arrays;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
+
         Arrays.sort(participant);
         Arrays.sort(completion);
-        for (int i=0; i<completion.length; i++) {
-            if (participant[i] != completion[i]) {
-                answer = participant[i];
-                break;
-            }
-        }
-        if (answer == "") {
-            answer += participant[participant.length-1];
-        }
-        return answer;
-    }
 
-    public static void main(String[] args) {
+        int i = 0;
+        for(i=0;i<completion.length;i++)
+            if(!participant[i].equals(completion[i]))
+                break;
+
+
+        return participant[i];
+    }
+    public static void main(String[] args){
         Solution s = new Solution();
-        String[] participant = {"mislav", "stanko", "mislav", "ana"};
-        String[] completion = {"stanko", "ana", "mislav"};
-        System.out.println(s.solution(participant, completion));
+        String[] part = {"leo", "kiki", "eden"};
+        String[] comp = {"eden", "kiki"};
+        System.out.println(s.solution(part, comp));
     }
 }
 */
