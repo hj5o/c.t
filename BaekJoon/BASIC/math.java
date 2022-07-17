2e0699604a5e7610814223de7b34c51adac2ede8
hj5o <dataliteracy@naver.com>
2022-07-17 22:00:54 +0900

    13909 창문 닫기

--- BaekJoon/BASIC/math.java
+++ BaekJoon/BASIC/math.java
@@ -874,6 +874,22 @@ public class Main {
         }
     }
 }
+
+       Problem 13909 창문 닫기
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int cnt = 0;
+        for (int i=1; i<=Math.sqrt(N); i++) {
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
+
        Problem 14681 사분면 고르기
 import java.util.Scanner;
 
