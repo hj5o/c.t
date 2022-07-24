04a6c54cd775004ad7507068d003ba775191bfde
hj5o <dataliteracy@naver.com>
2022-07-24 19:50:55 +0900

    1940 주몽

--- BaekJoon/BASIC/sort.java
+++ BaekJoon/BASIC/sort.java
@@ -209,6 +209,30 @@ public class Main {
     }
 }
 
+       Problem 1940 주몽
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int cnt = 0;
+        int[] arr = new int[N];
+        for (int i=0; i<N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i=0; i<N-1; i++) {
+            for (int j=i+1; j<N; j++) {
+                if (arr[i] + arr[j] == M) {
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
+
        Problem 2693 N번째 큰 수
 import java.io.BufferedReader;
 import java.io.IOException;
