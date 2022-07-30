52b24cfe760c2dacca93754c06ab9128eb169d9e
hj5o <dataliteracy@naver.com>
2022-07-30 20:40:50 +0900

    14425 문자열 집합

--- BaekJoon/BASIC/string.java
+++ BaekJoon/BASIC/string.java
@@ -86,4 +86,28 @@ public class Main {
     }
 }
 
+       Problem 14425 문자열 집합
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        HashSet<String> set = new HashSet<>();
+        int cnt = 0;
+        for (int i=0; i<N; i++) {
+            set.add(sc.next());
+        }
+        for (int i=0; i<M; i++) {
+            String str = sc.next();
+            if (set.contains(str)) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
+
 */
