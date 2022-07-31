5c4da212ef2117fb4710394280687a939d83456b
hj5o <dataliteracy@naver.com>
2022-07-31 22:29:10 +0900

    1269 대칭 차집합

--- BaekJoon/BASIC/math.java
+++ BaekJoon/BASIC/math.java
@@ -193,6 +193,31 @@ public class Main {
     }
 }
 
+       Problem 1269 대칭 차집합
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        HashSet<Integer> set = new HashSet<>();
+        for (int i=0; i<N; i++) {
+            set.add(sc.nextInt());
+        }
+        for (int i=0; i<M; i++) {
+            int L = sc.nextInt();
+            if (set.contains(L)) {
+                set.remove(L);
+            } else {
+                set.add(L);
+            }
+        }
+        System.out.println(set.size());
+    }
+}
+
        Problem 1292 쉽게 푸는 문제
 import java.util.ArrayList;
 import java.util.List;
