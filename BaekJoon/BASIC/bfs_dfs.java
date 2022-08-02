83bfa203dcf5bada449a51bb38c51207149f4929
hj5o <dataliteracy@naver.com>
2022-08-02 20:18:16 +0900

    9372 상근이의 여행

--- BaekJoon/BASIC/bfs_dfs.java
+++ BaekJoon/BASIC/bfs_dfs.java
@@ -269,6 +269,28 @@ public class Main {
     }
 }
 
+       Problem 9372 상근이의 여행
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int tc=0; tc<T; tc++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            int[][] board = new int[M][2];
+            for (int i=0; i<M; i++) {
+                for (int j=0; j<2; j++) {
+                    board[i][j] = sc.nextInt();
+                }
+            }
+            System.out.println(N-1);
+        }
+
+    }
+}
+
        Problem 14716 현수막
 import java.util.Scanner;
 
