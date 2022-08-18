f23e7e48e167c06c8f97ba3e251c79759826c557
hj5o <dataliteracy@naver.com>
2022-08-18 21:20:45 +0900

    4949 균형잡힌 세상

--- BaekJoon/BASIC/queue_stack_graph.java
+++ BaekJoon/BASIC/queue_stack_graph.java
@@ -76,6 +76,46 @@ public class Main {
     }
 }
 
+       Problem 4949 균형잡힌 세상
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String s = sc.nextLine();
+            if(s.equals(".")) {
+                break;
+            }
+            Stack<Character> stack = new Stack<>();
+            for (int i=0; i<s.length(); i++) {
+                char c = s.charAt(i);
+                if (c == '(' || c == '[') {
+                    stack.push(c);
+                } else if (c == ')') {
+                    if (!stack.isEmpty() && stack.peek().equals('(')) {
+                        stack.pop();
+                    } else {
+                        stack.push(c);
+                    }
+                } else if (c == ']') {
+                    if (!stack.isEmpty() && stack.peek().equals('[')) {
+                        stack.pop();
+                    } else {
+                        stack.push(c);
+                    }
+                }
+            }
+            if (stack.isEmpty()) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+}
+
        Problem 9012 괄호
 import java.io.BufferedReader;
 import java.io.IOException;
