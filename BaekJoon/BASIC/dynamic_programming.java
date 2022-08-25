cbcf1dabb19dc57055b672f612d93f17160ecb5f
hj5o <dataliteracy@naver.com>
2022-08-25 20:50:51 +0900

    15988 1,2,3 더하기 3

--- BaekJoon/BASIC/dynamic_programming.java
+++ BaekJoon/BASIC/dynamic_programming.java
@@ -496,5 +496,28 @@ public class Main {
         System.out.println(dp[N]);
     }
 }
+
+       Problem 15988 1,2,3 더하기 3
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long dp[] = new long[1000001];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for(int i=4; i<=1000000; i++) {
+            dp[i] = (dp[i-1] + dp[i-2] + dp[i-3]) % 1000000009;
+        }
+
+        int T = sc.nextInt();
+        for(int i=0; i<T; i++) {
+            int N = sc.nextInt();
+            System.out.println(dp[N]);
+        }
+    }
+}
 */
 
