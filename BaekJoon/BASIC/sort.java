eeec31eb226496436c16cf4047ed75784ddb1f75
hj5o <dataliteracy@naver.com>
2022-09-23 23:31:39 +0900

    25305 커트라인

--- BaekJoon/BASIC/sort.java
+++ BaekJoon/BASIC/sort.java
@@ -778,4 +778,21 @@ public class Main {
     }
 }
 
+       Problem 25305 커트라인
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i=0; i<N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[arr.length-M]);
+    }
+}
 */
