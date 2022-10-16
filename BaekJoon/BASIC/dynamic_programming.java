2efe9bae10025b19d17fe1df53321c0a26b320c9
hj5o <dataliteracy@naver.com>
2022-10-16 23:24:33 +0900

    11048 이동하기

--- BaekJoon/BASIC/dynamic_programming.java
+++ BaekJoon/BASIC/dynamic_programming.java
@@ -425,6 +425,30 @@ public class Main {
     }
 }
 
+       Problem 11048 이동하기
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[][] board = new int[N+1][M+1];
+        int[][] dp = new int[N+1][M+1];
+        for (int i=1; i<=N; i++) {
+            for (int j=1; j<=M; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+        for (int i=1; i<=N; i++) {
+            for (int j=1; j<=M; j++) {
+                dp[i][j] = Math.max(board[i][j] + dp[i-1][j], board[i][j] + dp[i][j-1]);
+            }
+        }
+        System.out.println(dp[N][M]);
+    }
+}
+
        Problem 14494 다이나믹이 뭐예요?
 import java.util.Scanner;
 
