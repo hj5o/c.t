d3e0a618feee990665156e252cc14151b39dfcf7
hj5o <dataliteracy@naver.com>
2022-10-21 23:31:29 +0900

    11060 점프 점프

--- BaekJoon/Problem/dynamic_programming.java
+++ BaekJoon/Problem/dynamic_programming.java
@@ -477,6 +477,33 @@ public class Main {
     }
 }
 
+       Problem 11060 점프 점프
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+        for (int i=0; i<N; i++) {
+            arr[i] = sc.nextInt();
+            dp[i] = -1;
+        }
+        dp[0] = 0;
+        for (int i=1; i<N; i++) {
+            for (int j=0; j<i; j++) {
+                if (dp[j] != -1 && i-j <= arr[j]) {
+                    if (dp[i] == -1 || dp[i] > dp[j] + 1 ) {
+                        dp[i] = dp[j] + 1;
+                    }
+                }
+            }
+        }
+        System.out.println(dp[N-1]);
+    }
+}
+
        Problem 14494 다이나믹이 뭐예요?
 import java.util.Scanner;
 
