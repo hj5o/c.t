ab336c3df2edbd9c59f26d0e995f9ec29545c483
hj5o <dataliteracy@naver.com>
2022-11-08 23:52:11 +0900

    2775 부녀회장이 될테야

--- BaekJoon/Problem/dynamic_programming.java
+++ BaekJoon/Problem/dynamic_programming.java
@@ -370,6 +370,31 @@ public class Main {
     }
 }
 
+       Problem 2775 부녀회장이 될테야
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int[][] apt = new int[15][15];
+        for (int i=1; i<15; i++) {
+            apt[i][1] = 1;
+            apt[0][i] = i;
+        }
+        for (int i=1; i<15; i++) {
+            for (int j=1; j<15; j++) {
+                apt[i][j] = apt[i][j-1] + apt[i-1][j];
+            }
+        }
+        for (int tc=0; tc<T; tc++) {
+            int k = sc.nextInt();
+            int n = sc.nextInt();
+            System.out.println(apt[k][n]);
+        }
+    }
+}
+
        Problem 9084 동전
 import java.util.*;
 
