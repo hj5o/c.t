0d84cd02316de5a6bdb15fd028632ff1787b2750
hj5o <dataliteracy@naver.com>
2022-11-10 23:46:31 +0900

    13301 타일 장식물

--- BaekJoon/Problem/dynamic_programming.java
+++ BaekJoon/Problem/dynamic_programming.java
@@ -577,6 +577,23 @@ public class Main {
     }
 }
 
+       Problem 13301 타일 장식물
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long[] dp = new long[N+1];
+        dp[0] = 2;
+        dp[1] = 4;
+        for(int i=2; i<N+1; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        System.out.println(dp[N]);
+    }
+}
+
        Problem 14494 다이나믹이 뭐예요?
 import java.util.Scanner;
 
