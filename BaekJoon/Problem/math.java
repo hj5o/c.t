a158774aa329921a86a198c234528e5b0e07bdea
hj5o <dataliteracy@naver.com>
2022-11-23 23:02:36 +0900

    10709 기상캐스터

--- BaekJoon/Problem/math.java
+++ BaekJoon/Problem/math.java
@@ -1026,6 +1026,42 @@ public class Main {
     }
 }
 
+       Problem 10709 기상캐스터
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int H = sc.nextInt();
+        int W = sc.nextInt();
+        int[][] board = new int[H][W];
+        for (int i=0; i<H; i++) {
+            for (int j=0; j<W; j++) {
+                board[i][j] = -1;
+            }
+        }
+        for (int i=0; i<H; i++) {
+            String s = sc.next();
+            char[] str = s.toCharArray();
+            for (int j=0; j<W; j++) {
+                if (str[j] == 'c') {
+                    int cnt = 0;
+                    for (int k=j; k<W; k++) {
+                        board[i][k] = cnt;
+                        cnt++;
+                    }
+                }
+            }
+        }
+        for (int i=0; i<H; i++) {
+            for (int j=0; j<W; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
        Problem  10818 최소, 최대
 import java.io.BufferedReader;
 import java.io.IOException;
